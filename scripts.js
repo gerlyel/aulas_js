dddb506088969a6d6b1f330276e7ba4f136ab58b
Mário <gerlyel123@gmail.com>
2021-01-13 11:22:06 -0300

    chamando as propredades do objeto no mesmo console.log

--- scripts.js
+++ scripts.js
@@ -16,3 +16,9 @@ console.log(person.name)
 console.log(person.age)
 console.log(person.weight)
 console.log(person.isAdmin)
+
+
+//pegar propriedades no mesmo console.log
+
+
+console.log(`${person.name} tem ${person.age} anos, e pesa ${person.weight}Kg`)
