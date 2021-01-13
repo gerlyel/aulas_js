0344946197a52e340e85d0127cf78c9115358431
Mário <gerlyel123@gmail.com>
2021-01-13 12:44:24 -0300

    entendendo arrays

---
+++ arrays.js
@@ -0,0 +1,22 @@
+//Arrays
+
+
+const animals = [
+    'lion',
+    'monkey',
+    'cat',
+    'snake',
+    'dog'
+]
+
+
+//Acessando os valores do array
+
+console.log(animals[2]) 
+                            /* Para escolher qual dado do Arrey usar, 
+                            entre colchetes [] coloque o número que 
+                            representa sua posição.. 
+                            sempre começando a contar do 0*/
+
+
+
