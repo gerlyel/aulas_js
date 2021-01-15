6f63ab5c25e7c617ba869d6dbf3ecabdae454db7
Mário <gerlyel123@gmail.com>
2021-01-15 08:53:27 -0300

    Manipulação de dados, descobrir a quantidades de caracteres de uma String/Number

---
+++ prototypes/manipulacaoDeDados.js
@@ -0,0 +1,12 @@
+//Manipulando Strings e Numbers
+
+
+//Contar quantos caracteres tem em uma palavra e quantos digitos tem em um número
+
+let word = 'institucionalizado'
+console.log(word.length) //O .length diz a qunatidades de caracteres de uma String
+
+let number = 123456
+console.log(String(number).length)/* O .length não diz a quantidade de caracteres de um Type number, 
+                                     para saber a quantidades de caracteres de uma variavel do tipo number
+                                     tem que fazer a conversão da variavel number(do tipo Number), para o Type String*/
