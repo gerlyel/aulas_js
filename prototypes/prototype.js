f253c239e213bcc69483eff6f9608dfe7c970dc8
Mário <gerlyel123@gmail.com>
2021-01-15 08:23:34 -0300

    Aprendendo sobre Type convertion

---
+++ prototypes/prototype.js
@@ -0,0 +1,12 @@
+/*
+        Type conversion (typecasting) vs Type coersion
+
+        * Alteração de um tipo de dado para outro tipo
+
+
+        console.log('9' + 5) Vai concatenar e a saída de dado vai ser: 95
+
+        Se antes da String '9' for adicionado um Number(), o type String '9' vai virar um type Number ocorredo uma conversão de dados
+        
+        por exemplo console.log(Number('9') + 5) a saída de dado vai ser uma soma resultando no resultado: 14
+*/
