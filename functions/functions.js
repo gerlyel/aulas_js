6b9b1397715db09e0ef26358b668fa847ffbd17e
Mário <gerlyel123@gmail.com>
2021-01-15 04:48:30 -0300

    aprendendo a usar o return e como chamar uma function interpolada

--- functions/functions.js
+++ functions/functions.js
@@ -8,3 +8,15 @@ const sum = function(number1, number2){
 }
 
 sum(2, 3) //argumentos
+
+
+const sum1 = function(number3, number4) {
+    total = number3 + number4
+    return total;
+}
+let number3 = 24
+let number4 = 35
+
+console.log(`O terceiro número é ${number3}`)
+console.log(`O quarto númrero é ${number4}`)
+console.log(`A soma do terceiro número ${number3} e do quarto número ${number4} é ${sum1(number3,number4)}`)
