b7c8ff666828ddd4e4cbf8832629b168a316dcee
Mário <gerlyel123@gmail.com>
2021-01-15 09:02:03 -0300

    Manipulando a quantidade de casas decimais em um número

--- prototypes/manipulacaoDeDados.js
+++ prototypes/manipulacaoDeDados.js
@@ -10,3 +10,10 @@ let number = 123456
 console.log(String(number).length)/* O .length não diz a quantidade de caracteres de um Type number, 
                                      para saber a quantidades de caracteres de uma variavel do tipo number
                                      tem que fazer a conversão da variavel number(do tipo Number), para o Type String*/
+
+
+//Transfrmando um número quebrado com 2 casas decimais e trocar . pela , 
+
+let n = 827.10910385
+
+console.log(n.toFixed(2).replace(".", ","))
