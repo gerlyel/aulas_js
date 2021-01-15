66cfca9f2037b463034886b57ff17ff3753bdc38
Mário <gerlyel123@gmail.com>
2021-01-15 05:06:16 -0300

    Entendendo como funciona o scope de uma function

--- functions/functions.js
+++ functions/functions.js
@@ -20,3 +20,18 @@ let number4 = 35
 console.log(`O terceiro número é ${number3}`)
 console.log(`O quarto númrero é ${number4}`)
 console.log(`A soma do terceiro número ${number3} e do quarto número ${number4} é ${sum1(number3,number4)}`)
+
+
+
+//function scope
+
+let pensamento = 'Estou com fome!'                //essa variavel pensamento 
+                                // é diferente
+function pensar(pensamento){   //desse parametro pensamento
+    pensamento = 'Estou com sede!'
+    return pensamento
+}
+
+console.log(pensar(pensamento)) //a saida de dado desse console.log vai ser: 'Estou com sede!'
+console.log(pensamento) //já a saida de dado desse console.log vai ser: 'Estou com fome!'
+                        //pois o pensamento da function esta dentro de um escopo, então só será chamada caso a function seja chamada
