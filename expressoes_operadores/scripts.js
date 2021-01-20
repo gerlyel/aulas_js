c4f62c0882460deb7e3ee583ba170212030f19c1
gerlyel <gerlyel123@gmail.com>
2021-01-20 10:13:43 -0300

    Crando objetos com a expression new

---
+++ expressoes_operadores/scripts.js
@@ -0,0 +1,31 @@
+/* Expressões e Operadores 
+
+- Expressions
+- Operators
+    Binary
+    Unary
+    Ternary
+
+*/ 
+
+let number //expressões é qualquer linha de código que resolve alguma coisa no Js
+
+// Expresion new
+/*
+    * left-hand-side expression
+    * criar um novo objeto
+
+ */
+
+ let nome= new String('Mário')
+ nome.surName = 'Gerlyel'
+ let age = new Number(18)
+
+ console.log(nome.surName, age)
+
+let data = new Date('2021-01-20')
+
+console.log(data)
+
+
+
