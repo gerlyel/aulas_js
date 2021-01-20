f9975a7ec1c5ace937a5b62b1fc28c6666357164
gerlyel <gerlyel123@gmail.com>
2021-01-20 09:31:56 -0300

    manipulando elementos dentro do array

--- prototypes/manipulacaoDeDados.js
+++ prototypes/manipulacaoDeDados.js
@@ -58,3 +58,22 @@ console.log([1, 2, 3, 4, 5, "a", "b", "C"].length)
 
 let Word = "Manipulação"
 console.log(Array.from(Word))
+
+let techs = ["html", "css", "js"]
+
+//adicionar um item no fim
+techs.push("node.js")
+//adicionar um item no começo
+techs.unshift("sql")
+// remover no fim
+techs.pop()
+//remover no começo
+techs.shift()
+//pegar somente alguns elementos do array
+console.log(techs.slice(1, 3))
+//remover 1 ou mais items em qualquer posição do array
+techs.splice(1, 2)
+//encontrar a posição de um elemento no array
+let index = techs.indexOf('css')
+
+console.log(index)
