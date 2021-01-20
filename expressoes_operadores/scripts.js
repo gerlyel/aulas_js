68c9941d323096ba4f91c9a65c545f42b2ce3bcd
gerlyel <gerlyel123@gmail.com>
2021-01-20 10:53:45 -0300

    Entendendo os operatores lógicos

--- expressoes_operadores/scripts.js
+++ expressoes_operadores/scripts.js
@@ -39,4 +39,35 @@ delete person.age
 
 console.log(person)
 
+/*
+    FALSY:
+    Quando u  valor é considerado false em contextos onde um booleano é obrigatório (condicionais e loops)
 
+    false
+    0
+    -0
+    ""
+    null
+    undefined
+    NaN
+*/
+console.log(0? 'verdadeiro' : 'Falso')
+
+/*
+    TRUTHY
+    Quando um valor é considerado true em contextos onde um booleano é obrigatório ( condicionais e loops)
+
+    true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    infinity
+    -infinity
+
+*/
+
+console.log( {} ? 'verdadeiro' : 'falso')
