48d1042cadf53126de5e49f6d83f15944a6dbd55
gerlyel <gerlyel123@gmail.com>
2021-01-20 08:14:05 -0300

    manipulando string e arrays

--- prototypes/manipulacaoDeDados.js
+++ prototypes/manipulacaoDeDados.js
@@ -32,3 +32,13 @@ console.log(string.toLowerCase())   //Tudo dentro da string fica minúsculo/cach
 let frase = 'Eu canto porque o instante existe e a minha vida está completa. Não sou alegre nem sou triste: sou poeta. Irmão das coisas fugidias, não sinto gozo nem tormento. Atravesso noites e dias no vento. Se desmorono ou se edifico, se permaneço ou me desfaço, — não sei, não sei. Não sei se fico ou passo. Sei que canto. E a canção é tudo. Tem sangue eterno a asa ritmada. E um dia sei que estarei mudo: — mais nada.'
 
 console.log(frase.includes('canto'))
+
+
+
+//Manipulando strings e arrays
+
+let phrase = "Eu quero viver o amor!"
+let myArray = phrase.split(" ")
+
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore)
