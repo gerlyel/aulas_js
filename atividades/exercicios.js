4539582d53b33ed8b517ee62278dc3cfb5081ce2
gerlyel <gerlyel123@gmail.com>
2021-01-22 04:36:46 -0300

    Exercício 2: sistemas de gastos familiar

--- atividades/exercicios.js
+++ atividades/exercicios.js
@@ -39,7 +39,46 @@ console.log(getScore(103))
 console.log(getScore(-70))
 
 
+/*
+    Exercício 2:
+
+        SISTEMA DE GASTOS FAMILIAR
+
+ */
+
+ let gastos = {
+     receitas: [200, 40.60, 1000.30],
+     despesas: [300, 80.00, 295.40]
+ }
+
+function soma(array) {
+    let total = 0;
+
+    for(let value of array){
+        total += value
+    }
+
+    return total
+}
+
+ function mostrarGastos() {
+    const totalReceitas = soma(gastos.receitas)
+    const totalDespesas = soma(gastos.despesas)
 
 
+    const total = totalReceitas - totalDespesas
+
+    const ok = total >= 0
+
+    let menssagem = 'NEGATIVO'
+
+    if(ok){
+        menssagem = 'POSITIVO'
+    }
+    
+    console.log(`Seu saldo é ${menssagem}: ${total}`)
+ }
+
+ mostrarGastos()
 
 
